d12342aa261cc8193948486bab7a08e1442da974
SandraGargiulo <sandragargiulo@gmail.com>
2022-06-16 23:34:22 -0300

    texto en itemlistconteiner

--- src/components/ItemListContainer.jsx
+++ src/components/ItemListContainer.jsx
@@ -7,8 +7,10 @@ const ItemListConteiner = ({ title }) => {
             <div className="ItemListConteiner-conteiner">
                 <article className="ItemListConteiner__text-box">
                     <h1>{title}</h1>
+                    <h2>Grupos electrogenos</h2>
                     <p>
-                        
+                        Venta
+                        Alquiler
                     </p>
                     <button className="button-primary">Comprar</button>
                 </article>
